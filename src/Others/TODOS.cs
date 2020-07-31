17ab5757bde1f647209e696eedfb6be4e5efb450
Paulo Barbeiro <paulo_barbeiro04@hotmail.com>
2020-07-31 22:18:05 +0300

    Minor changes

--- src/Others/TODOS.cs
+++ src/Others/TODOS.cs
@@ -8,8 +8,6 @@
         - Write code to post a 'Secret' with the credentials used for login
         - Modify Web app code to provide feed back when user has already been registered
         - Modify Web app code to provide feedback when a user tries to login/register with empty strings (?) 
-        - Organize code
-        - Organize TODOs lol
         - Assign appropriate return values for methods
         */
 
