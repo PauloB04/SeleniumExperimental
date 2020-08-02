a756170d3b98101783d59ad317a8108765945342
Paulo Barbeiro <paulo_barbeiro04@hotmail.com>
2020-08-03 01:06:59 +0300

    added one TODO

--- src/Others/TODOS.cs
+++ src/Others/TODOS.cs
@@ -6,6 +6,7 @@
         TODO: 
         - Review & Organize code into different classes
         - review the use of waitGlobal and driverGlobal, maybe you don't need to pass  'driver' references on methods
+        - Find the updated version of ExpectedConditions as it is deprecated
         - Modify Web app code to provide feed back when user has already been registered
         - Modify Web app code to provide feedback when a user tries to login/register with empty strings (?) 
         - Assign appropriate return values for methods
